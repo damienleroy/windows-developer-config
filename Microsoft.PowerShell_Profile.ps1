af29bede094d911e0b0d41803c4d1d91e16f922b
damienleroy <13002713+damienleroy@users.noreply.github.com>
2022-09-22 18:04:47 +0200

    Add files via upload

--- Microsoft.PowerShell_Profile.ps1
+++ Microsoft.PowerShell_Profile.ps1
@@ -14,10 +14,10 @@ Set-PoshPrompt perso
 Function CBIN_FN {Get-ChildItem .\ -include bin,obj -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName -Force -Recurse }}
 Set-Alias -Name cbin -Value CBIN_FN
 
-Function TOOLS_FN {cd C:\Users\dleroy\source\repos\tools}
+Function TOOLS_FN {cd C:\Users\dleroy\source\repos\tools; $host.UI.RawUI.WindowTitle = "Tools"}
 Set-Alias -Name tools -Value TOOLS_FN
 
-Function TEST_FN {cd C:\Users\dleroy\source\repos\test}
+Function TEST_FN {cd C:\Users\dleroy\source\repos\test; $host.UI.RawUI.WindowTitle = "Tests"}
 Set-Alias -Name test -Value TEST_FN
 
 
@@ -25,14 +25,22 @@ Set-Alias -Name test -Value TEST_FN
 Function NAV_FN {cd C:\Users\dleroy\source\repos\navigation}
 Set-Alias -Name nav -Value NAV_FN
 
-Function NAV_API_FN {cd C:\Users\dleroy\source\repos\navigation\api}
+Function NAV_API_FN {cd C:\Users\dleroy\source\repos\navigation\api; $host.UI.RawUI.WindowTitle = "Nav API"}
 Set-Alias -Name api -Value NAV_API_FN
 
-Function NAV_MS_FN {cd C:\Users\dleroy\source\repos\navigation\microservices}
+Function NAV_MS_FN {cd C:\Users\dleroy\source\repos\navigation\microservices; $host.UI.RawUI.WindowTitle = "Microservices"}
 Set-Alias -Name microservices -Value NAV_MS_FN
 
-Function NAV_OA_FN {cd C:\Users\dleroy\source\repos\navigation\operation-access}
+Function NAV_OA_FN {cd C:\Users\dleroy\source\repos\navigation\operation-access; $host.UI.RawUI.WindowTitle = "OperationAccess"}
 Set-Alias -Name operation-access -Value NAV_OA_FN
 
-Function NAV_BOOM_FN {cd C:\Users\dleroy\source\repos\navigation\boom}
+Function NAV_BOOM_FN {cd C:\Users\dleroy\source\repos\navigation\boom; $host.UI.RawUI.WindowTitle = "Boom"}
 Set-Alias -Name boom -Value NAV_BOOM_FN
+
+Function NAV_DATAINJECTOR_FN {cd C:\Users\dleroy\source\repos\navigation\datainjector; $host.UI.RawUI.WindowTitle = "DataInjector"}
+Set-Alias -Name datainjector -Value NAV_DATAINJECTOR_FN
+
+# helper to turn PSCustomObject into a list of key/value pairs
+function GetlaunchSettingsEnvironmentVariable {
+    (Get-Content .\Properties\launchSettings.json | Out-String | ConvertFrom-Json).profiles."OperationAccess.Api".environmentVariables.ASPNETCORE_ENVIRONMENT
+}
